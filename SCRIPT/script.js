ceb9d63e0c55a0dea4fe25e42a3e61c00cc8554d
GUIFREESANShub <guilherme.frigeri.santos@escola.pr.gov.br>
2026-05-04 11:04:47 -0300

    h

---
+++ SCRIPT/script.js
@@ -0,0 +1,12 @@
+function salvarCarros(event){
+    event.preventDefault ();
+
+    let título = document.getElementById('title').value;
+
+    let preco  = document.getElementById('preço').value;
+
+    let marca  = document.getElementById('marca').Value;
+
+    let modelo = document.getElementById('modelo').Value;
+        
+}
